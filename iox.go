2cb44c179f4770990c823a85336a2a67553fe446
yudeguang <84558040@qq.com>
2018-01-30 09:06:24 +0800

    增加ReadByteRemaining等方法

--- iox.go
+++ iox.go
@@ -1,5 +1,5 @@
 //主要用于在文件(os.File)中查找相关字节数组 Index(b []byte) 以及读取数据 ReadByte(n int)等操作，同时也支持所有io.ReadSeeker类型数据
-package iox
+package files
 
 import (
 	"bytes"
@@ -12,20 +12,20 @@ import (
 	"strings"
 )
 
-//IoReadSeeker及为io.ReadSeeker
-type IoReadSeeker struct {
+//ReadSeeker及为io.ReadSeeker
+type ReadSeeker struct {
 	readSeeker io.ReadSeeker
 }
 
-//从io.readSeeker初始化
-func New(rs io.ReadSeeker) *IoReadSeeker {
-	f := new(IoReadSeeker)
+//从io.ReadSeeker初始化
+func New(rs io.ReadSeeker) *ReadSeeker {
+	f := new(ReadSeeker)
 	f.readSeeker = rs
 	return f
 }
 
 //从文件初始化,注意最后要调用Close关闭文件
-func NewFromFile(fileName string) (*IoReadSeeker, error) {
+func NewFromFile(fileName string) (*ReadSeeker, error) {
 	file, err := os.OpenFile(fileName, os.O_RDONLY, 0x666)
 	if err != nil {
 		return nil, err
@@ -35,19 +35,19 @@ func NewFromFile(fileName string) (*IoReadSeeker, error) {
 }
 
 //从切片初始化
-func NewFromBytes(b []byte) *IoReadSeeker {
+func NewFromBytes(b []byte) *ReadSeeker {
 	return New(bytes.NewReader(b))
 }
 
 //关闭f
-func (f *IoReadSeeker) Close() {
+func (f *ReadSeeker) Close() {
 	if closer, ok := f.readSeeker.(io.Closer); ok {
 		closer.Close()
 	}
 }
 
 //从当前位置移动多少个字节
-func (f *IoReadSeeker) Move(n int64) error {
+func (f *ReadSeeker) Move(n int64) error {
 	_, err := f.readSeeker.Seek(n, io.SeekCurrent)
 	if err != nil {
 		return err
@@ -56,7 +56,7 @@ func (f *IoReadSeeker) Move(n int64) error {
 }
 
 //移动到某个位置
-func (f *IoReadSeeker) MoveTo(pos int64) error {
+func (f *ReadSeeker) MoveTo(pos int64) error {
 	_, err := f.readSeeker.Seek(pos, io.SeekStart)
 	if err != nil {
 		return err
@@ -65,12 +65,12 @@ func (f *IoReadSeeker) MoveTo(pos int64) error {
 }
 
 //获得当前位置
-func (f *IoReadSeeker) CurPos() (int64, error) {
+func (f *ReadSeeker) CurPos() (int64, error) {
 	return f.readSeeker.Seek(0, io.SeekCurrent)
 }
 
 //获得长度
-func (f *IoReadSeeker) Len() int64 {
+func (f *ReadSeeker) Len() int64 {
 	//防止文件位置发生移动
 	initialPos, _ := f.CurPos()
 	defer f.MoveTo(initialPos)
@@ -79,7 +79,7 @@ func (f *IoReadSeeker) Len() int64 {
 }
 
 //读取n个字节,并移动指针
-func (f *IoReadSeeker) ReadByte(n int) ([]byte, error) {
+func (f *ReadSeeker) ReadByte(n int) ([]byte, error) {
 	currentPos, err := f.CurPos()
 	if err != nil {
 		return nil, err
@@ -107,8 +107,17 @@ func (f *IoReadSeeker) ReadByte(n int) ([]byte, error) {
 	return bt, nil
 }
 
+//从当前位置起，读取剩余的
+func (f *ReadSeeker) ReadByteRemaining() ([]byte, error) {
+	currentPos, err := f.CurPos()
+	if err != nil {
+		return nil, err
+	}
+	return f.ReadByte(int(f.Len() - currentPos))
+}
+
 //读取若干字节,并移动指针,其中第1个字节表示后续待读取切片长度
-func (f *IoReadSeeker) ReadByteUint8() ([]byte, error) {
+func (f *ReadSeeker) ReadByteUint8() ([]byte, error) {
 	n, err := f.ReadUint8()
 	if err != nil {
 		return nil, err
@@ -117,7 +126,7 @@ func (f *IoReadSeeker) ReadByteUint8() ([]byte, error) {
 }
 
 //读取若干字节,并移动指针,其中前2个字节表示后续待读取切片长度
-func (f *IoReadSeeker) ReadByteUint16() ([]byte, error) {
+func (f *ReadSeeker) ReadByteUint16() ([]byte, error) {
 	n, err := f.ReadUint16()
 	if err != nil {
 		return nil, err
@@ -126,7 +135,7 @@ func (f *IoReadSeeker) ReadByteUint16() ([]byte, error) {
 }
 
 //读取若干字节,并移动指针,其中前2个字节表示后续待读取切片长度
-func (f *IoReadSeeker) ReadByteUint16BigEndian() ([]byte, error) {
+func (f *ReadSeeker) ReadByteUint16BigEndian() ([]byte, error) {
 	n, err := f.ReadUint16BigEndian()
 	if err != nil {
 		return nil, err
@@ -135,7 +144,7 @@ func (f *IoReadSeeker) ReadByteUint16BigEndian() ([]byte, error) {
 }
 
 //读取若干字节,并移动指针,其中前4个字节表示后续待读取切片长度
-func (f *IoReadSeeker) ReadByteUint32() ([]byte, error) {
+func (f *ReadSeeker) ReadByteUint32() ([]byte, error) {
 	n, err := f.ReadUint32()
 	if err != nil {
 		return nil, err
@@ -144,7 +153,7 @@ func (f *IoReadSeeker) ReadByteUint32() ([]byte, error) {
 }
 
 //读取若干字节,并移动指针,其中前4个字节表示后续待读取切片长度
-func (f *IoReadSeeker) ReadByteUint32BigEndian() ([]byte, error) {
+func (f *ReadSeeker) ReadByteUint32BigEndian() ([]byte, error) {
 	n, err := f.ReadUint32BigEndian()
 	if err != nil {
 		return nil, err
@@ -153,7 +162,7 @@ func (f *IoReadSeeker) ReadByteUint32BigEndian() ([]byte, error) {
 }
 
 //读取若干字节,并移动指针,其中前8个字节表示后续待读取切片长度
-func (f *IoReadSeeker) ReadByteUint64() ([]byte, error) {
+func (f *ReadSeeker) ReadByteUint64() ([]byte, error) {
 	n, err := f.ReadUint64()
 	if err != nil {
 		return nil, err
@@ -162,7 +171,7 @@ func (f *IoReadSeeker) ReadByteUint64() ([]byte, error) {
 }
 
 //读取若干字节,并移动指针,其中前8个字节表示后续待读取切片长度
-func (f *IoReadSeeker) ReadByteUint64BigEndian() ([]byte, error) {
+func (f *ReadSeeker) ReadByteUint64BigEndian() ([]byte, error) {
 	n, err := f.ReadUint64BigEndian()
 	if err != nil {
 		return nil, err
@@ -171,7 +180,7 @@ func (f *IoReadSeeker) ReadByteUint64BigEndian() ([]byte, error) {
 }
 
 //读取若干字节,转化为文本,并移动指针,其中第1个字节表示后续待读取切片长度
-func (f *IoReadSeeker) ReadStringUint8() (string, error) {
+func (f *ReadSeeker) ReadStringUint8() (string, error) {
 	n, err := f.ReadUint8()
 	if err != nil {
 		return "", err
@@ -180,7 +189,7 @@ func (f *IoReadSeeker) ReadStringUint8() (string, error) {
 }
 
 //读取若干字节,转化为文本,并移动指针,其中前2个字节表示后续待读取切片长度
-func (f *IoReadSeeker) ReadStringUint16() (string, error) {
+func (f *ReadSeeker) ReadStringUint16() (string, error) {
 	n, err := f.ReadUint16()
 	if err != nil {
 		return "", err
@@ -189,7 +198,7 @@ func (f *IoReadSeeker) ReadStringUint16() (string, error) {
 }
 
 //读取若干字节,转化为文本,并移动指针,其中前2个字节表示后续待读取切片长度
-func (f *IoReadSeeker) ReadStringUint16BigEndian() (string, error) {
+func (f *ReadSeeker) ReadStringUint16BigEndian() (string, error) {
 	n, err := f.ReadUint16BigEndian()
 	if err != nil {
 		return "", err
@@ -198,7 +207,7 @@ func (f *IoReadSeeker) ReadStringUint16BigEndian() (string, error) {
 }
 
 //读取若干字节,转化为文本,并移动指针,其中前4个字节表示后续待读取切片长度
-func (f *IoReadSeeker) ReadStringUint32() (string, error) {
+func (f *ReadSeeker) ReadStringUint32() (string, error) {
 	n, err := f.ReadUint32()
 	if err != nil {
 		return "", err
@@ -207,7 +216,7 @@ func (f *IoReadSeeker) ReadStringUint32() (string, error) {
 }
 
 //读取若干字节,转化为文本,并移动指针,其中前4个字节表示后续待读取切片长度
-func (f *IoReadSeeker) ReadStringUint32BigEndian() (string, error) {
+func (f *ReadSeeker) ReadStringUint32BigEndian() (string, error) {
 	n, err := f.ReadUint32BigEndian()
 	if err != nil {
 		return "", err
@@ -216,7 +225,7 @@ func (f *IoReadSeeker) ReadStringUint32BigEndian() (string, error) {
 }
 
 //读取若干字节,转化为文本,并移动指针,其中前8个字节表示后续待读取切片长度
-func (f *IoReadSeeker) ReadStringUint64() (string, error) {
+func (f *ReadSeeker) ReadStringUint64() (string, error) {
 	n, err := f.ReadUint64()
 	if err != nil {
 		return "", err
@@ -225,7 +234,7 @@ func (f *IoReadSeeker) ReadStringUint64() (string, error) {
 }
 
 //读取若干字节,转化为文本,并移动指针,其中前8个字节表示后续待读取切片长度
-func (f *IoReadSeeker) ReadStringUint64BigEndian() (string, error) {
+func (f *ReadSeeker) ReadStringUint64BigEndian() (string, error) {
 	n, err := f.ReadUint64BigEndian()
 	if err != nil {
 		return "", err
@@ -234,7 +243,7 @@ func (f *IoReadSeeker) ReadStringUint64BigEndian() (string, error) {
 }
 
 //读取n个字节的16进制数据,转化为string,并移动指针
-func (f *IoReadSeeker) ReadHexToString(n int) (string, error) {
+func (f *ReadSeeker) ReadHexToString(n int) (string, error) {
 	bt, err := f.ReadByte(n)
 	if err != nil {
 		return "", err
@@ -243,7 +252,7 @@ func (f *IoReadSeeker) ReadHexToString(n int) (string, error) {
 }
 
 //读取n个字节,转化为string,并移动指针
-func (f *IoReadSeeker) ReadString(n int) (string, error) {
+func (f *ReadSeeker) ReadString(n int) (string, error) {
 	bt, err := f.ReadByte(n)
 	if err != nil {
 		return "", err
@@ -251,8 +260,17 @@ func (f *IoReadSeeker) ReadString(n int) (string, error) {
 	return string(bt), nil
 }
 
+//从当前位置开始，读取剩余部分数据
+func (f *ReadSeeker) ReadStringRemaining() (string, error) {
+	bt, err := f.ReadByteRemaining()
+	if err != nil {
+		return "", err
+	}
+	return string(bt), nil
+}
+
 //读取n个字节,转化为去前后空格的string,并移动指针
-func (f *IoReadSeeker) ReadStringTrimSpace(n int) (string, error) {
+func (f *ReadSeeker) ReadStringTrimSpace(n int) (string, error) {
 	bt, err := f.ReadByte(n)
 	if err != nil {
 		return "", err
@@ -262,7 +280,7 @@ func (f *IoReadSeeker) ReadStringTrimSpace(n int) (string, error) {
 }
 
 //读取一个uint8数,并移动指针
-func (f *IoReadSeeker) ReadUint8() (uint8, error) {
+func (f *ReadSeeker) ReadUint8() (uint8, error) {
 	bt := make([]byte, 1)
 	_, err := f.readSeeker.Read(bt)
 	if err != nil {
@@ -272,7 +290,7 @@ func (f *IoReadSeeker) ReadUint8() (uint8, error) {
 }
 
 //读取一个uint16数,并移动指针
-func (f *IoReadSeeker) ReadUint16() (uint16, error) {
+func (f *ReadSeeker) ReadUint16() (uint16, error) {
 	bt := make([]byte, 2)
 	_, err := f.readSeeker.Read(bt)
 	if err != nil {
@@ -283,7 +301,7 @@ func (f *IoReadSeeker) ReadUint16() (uint16, error) {
 }
 
 //读取一个uint16数,并移动指针
-func (f *IoReadSeeker) ReadUint16BigEndian() (uint16, error) {
+func (f *ReadSeeker) ReadUint16BigEndian() (uint16, error) {
 	bt := make([]byte, 2)
 	_, err := f.readSeeker.Read(bt)
 	if err != nil {
@@ -294,7 +312,7 @@ func (f *IoReadSeeker) ReadUint16BigEndian() (uint16, error) {
 }
 
 //读取一个uint32数,并移动指针
-func (f *IoReadSeeker) ReadUint32() (uint32, error) {
+func (f *ReadSeeker) ReadUint32() (uint32, error) {
 	bt := make([]byte, 4)
 	_, err := f.readSeeker.Read(bt)
 	if err != nil {
@@ -305,7 +323,7 @@ func (f *IoReadSeeker) ReadUint32() (uint32, error) {
 }
 
 //读取一个uint32数,并移动指针
-func (f *IoReadSeeker) ReadUint32BigEndian() (uint32, error) {
+func (f *ReadSeeker) ReadUint32BigEndian() (uint32, error) {
 	bt := make([]byte, 4)
 	_, err := f.readSeeker.Read(bt)
 	if err != nil {
@@ -316,7 +334,7 @@ func (f *IoReadSeeker) ReadUint32BigEndian() (uint32, error) {
 }
 
 //读取一个uint64数,并移动指针
-func (f *IoReadSeeker) ReadUint64() (uint64, error) {
+func (f *ReadSeeker) ReadUint64() (uint64, error) {
 	bt := make([]byte, 8)
 	_, err := f.readSeeker.Read(bt)
 	if err != nil {
@@ -327,7 +345,7 @@ func (f *IoReadSeeker) ReadUint64() (uint64, error) {
 }
 
 //读取一个uint64数,并移动指针
-func (f *IoReadSeeker) ReadUint64BigEndian() (uint64, error) {
+func (f *ReadSeeker) ReadUint64BigEndian() (uint64, error) {
 	bt := make([]byte, 8)
 	_, err := f.readSeeker.Read(bt)
 	if err != nil {
@@ -338,17 +356,17 @@ func (f *IoReadSeeker) ReadUint64BigEndian() (uint64, error) {
 }
 
 //判断f是否包含子串sep
-func (f *IoReadSeeker) Contains(sep []byte) bool {
+func (f *ReadSeeker) Contains(sep []byte) bool {
 	return f.Index(sep) != -1
 }
 
 //计算f中共有多少个不重重叠的sep子串
-func (f *IoReadSeeker) Count(sep []byte) int64 {
+func (f *ReadSeeker) Count(sep []byte) int64 {
 	return f.CountGen(0, f.Len()-1, sep)
 }
 
 //判断f中某段数据内有多少个不重叠的sep子串
-func (f *IoReadSeeker) CountGen(beginPos, endPos int64, sep []byte) int64 {
+func (f *ReadSeeker) CountGen(beginPos, endPos int64, sep []byte) int64 {
 	//防止文件位置发生移动
 	initialPos, _ := f.CurPos()
 	defer f.MoveTo(initialPos)
@@ -380,14 +398,14 @@ func (f *IoReadSeeker) CountGen(beginPos, endPos int64, sep []byte) int64 {
 }
 
 //查找数据,从文件的开始位置开始查找sep子串,返回第1次出现的位置
-func (f *IoReadSeeker) Index(sep []byte) int64 {
+func (f *ReadSeeker) Index(sep []byte) int64 {
 	endPos := f.Len() - 1
 	return f.IndexGen(0, endPos, sep)
 }
 
 //查找数据,从文件的beginPos位置到endPos位置开始查找sep子串,返回第1次出现的位置
 //注意待搜索数据包含beginPos及endPos本身
-func (f *IoReadSeeker) IndexGen(beginPos, endPos int64, sep []byte) int64 {
+func (f *ReadSeeker) IndexGen(beginPos, endPos int64, sep []byte) int64 {
 	//防止文件位置发生移动
 	initialPos, _ := f.CurPos()
 	defer f.MoveTo(initialPos)
@@ -437,7 +455,7 @@ func (f *IoReadSeeker) IndexGen(beginPos, endPos int64, sep []byte) int64 {
 }
 
 //查找数据,从文件的beginPos位置开始查找S子串,第N次出现的位置,N大于0,beginPos大于等于0
-func (f *IoReadSeeker) IndexN(beginPos int64, sep []byte, n int) int64 {
+func (f *ReadSeeker) IndexN(beginPos int64, sep []byte, n int) int64 {
 	//防止文件位置发生移动
 	initialPos, _ := f.CurPos()
 	defer f.MoveTo(initialPos)
@@ -466,13 +484,13 @@ func (f *IoReadSeeker) IndexN(beginPos int64, sep []byte, n int) int64 {
 }
 
 //查找数据,在整个文件中查找,返回最后一次出现sep的位置,注意待搜索数据包含beginPos及endPos本身
-func (f *IoReadSeeker) LastIndex(sep []byte) int64 {
+func (f *ReadSeeker) LastIndex(sep []byte) int64 {
 	endPos := f.Len() - 1
 	return f.LastIndexGen(0, endPos, sep)
 }
 
 //查找数据,在的beginPos到endPos内查找sep子串,返回最后1次出现的位置,注意待搜索数据包含beginPos及endPos本身
-func (f *IoReadSeeker) LastIndexGen(beginPos, endPos int64, sep []byte) int64 {
+func (f *ReadSeeker) LastIndexGen(beginPos, endPos int64, sep []byte) int64 {
 	//防止文件位置发生移动
 	initialPos, _ := f.CurPos()
 	defer f.MoveTo(initialPos)
@@ -524,7 +542,7 @@ func (f *IoReadSeeker) LastIndexGen(beginPos, endPos int64, sep []byte) int64 {
 }
 
 //从当前位置向前读取n个字节,并移动指针
-func (f *IoReadSeeker) ReadByteReverse(n int) ([]byte, error) {
+func (f *ReadSeeker) ReadByteReverse(n int) ([]byte, error) {
 	//先向前移动n个位置
 	err := f.Move(int64(-n))
 	if err != nil {
